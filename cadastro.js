d496354cd846ba1578160f2dd8f80ac1c8fc216d
GuilhermeKulkamp <guilhermekulkamp@gmail.com>
2021-11-04 22:43:30 -0300

    criação da estrutura e formatação

---
+++ cadastro.js
@@ -0,0 +1,10 @@
+var itens = [];
+//
+//document.querySelector('input[type=submit]').addEventListener('click',()=>{
+//    alert("fui clicado")
+//});
+
+document.getElementById("cadastrar").addEventListener("click",()=>{
+    var produto = document.getElementById("nome_produto")
+    alert("fui clicado de novo");
+});
